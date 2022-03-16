3b9079c3b15a09df1cd7e74da1c8542376e33bae
wenxuanye <wey420@lehigh.edu>
2022-03-16 15:49:54 -0400

    version0.03

--- setup.py
+++ setup.py
@@ -5,17 +5,19 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name="test_package_wenxuanye",
-  version="0.0.2",
+  version="0.0.3",
   author="WENXUAN YE",
   author_email="lehighwenxuanye@gmail.com",
   description="A small example package",
   long_description=long_description,
   long_description_content_type="text/markdown",
   url="https://github.com/wenxuanye/testPackage.git",
-  packages=setuptools.find_packages(),
   classifiers=[
   "Programming Language :: Python :: 3",
   "License :: OSI Approved :: MIT License",
   "Operating System :: OS Independent",
   ],
+  package_dir={"": "src"},
+  packages = setuptools.find_packages(where="src"),
+  python_requires=">=3.6",
 )
